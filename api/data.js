a757990e2a662574129d87b3eada1b309ecfd104
kzmfhm <khuzaimanazik4@gmail.com>
2023-10-05 13:23:47 +0500

    updated data in api/data

--- api/data.js
+++ api/data.js
@@ -1,98 +1,50 @@
+// data.js
 const cards = [
   {
     username: "_frontend_developer",
     stories: [
       {
         id: "223c46e8-25a5-4e81-90c2-8ded2690db04",
-        storyLink: "/public/videos/code.mp4",
+        video: '/videos/tom-jery.mp4',
       },
-      {
-        id: "323bb349-9719-4e47-b90c-e7444e0528fc",
-        storyLink: "/public/images/animated.png",
-      },
-      {
-        id: "c7bd8411-d6b8-4e7d-a620-74117409e3e2",
-        storyLink: "/public/videos/frontend-course.mp4",
-      },
-      {
-        id: "88d88d4f-b9b2-4717-9ed0-e3ed2a7ff5d9",
-        storyLink: "/public/images/React.webp",
-      },
-   
     ],
   },
-    {
-      username: "kzm",
-      stories: [
-        {
-          id: "3ca09142-593e-4ea7-b0ec-9b840e3fbb35",
-          storyLink: "/public/images/comic01.jpg",
-        },
-        {
-          id: "3b4e0eae-ef47-468f-bb8a-b2c956968740",
-          storyLink: "/public/images/comic02.jpg",
-        },
-        {
-          id: "37e4c254-8cc7-4143-b6b3-403f4ecd7faf",
-          storyLink: "/public/images/comic03.jpg",
-        },
-        {
-          id: "5f07440b-c6f9-4025-8f16-05e95124c735",
-          storyLink: "/public/images/one.jpg",
-        },
-        {
-          id: "8f6d8ff8-48be-4464-a63e-4ef81801bb8d",
-          storyLink: "/public/images/two.jpg",
-        },
-      ],
-    },
-    {
-      username: "instagram",
-      stories: [
-        
-              {
-                id: "c9464dc4-1643-44ef-aa9a-978b3071c992",
-                storyLink: "/public/videos/tom-jery.mp4",
-              },
-             
-              {
-                id: "64a35eb5-754e-4ca3-9003-b3cd5f39abf6",
-                storyLink: "/public/videos/cute.mp4",
-              },
-
-          ],
-    },
-    {
-        username: "icc",
-        stories: [
-          {
-            id: "d2bfccaa-442b-4a25-9f01-b56941e13485",
-            storyLink: "/public/images/icc-1.jpg",
-          },
-          {
-            id: "c7bd8411-d6b8-4e7d-a620-74117409e3e2",
-            storyLink: "/public/images/icc-2.jpg",
-          },
-          {
-            id: "c7bd8411-d6b8-4e7d-a620-74117409e3e2",
-            storyLink: "/public/images/icc-3.webp",
-          },
-        ],
-      },
+  {
+    username: "kzm",
+    stories: [
       {
-        username: "pk",
-      stories: [
-        {
-          id: "5185e270-a319-4303-9624-c94f0fe5d2a6",
-          storyLink: "/public/videos/tom-jery.mp4",
-        },
-       
-      ],
-    },
-     
-     
-   
-  ];
-  
-  export default cards;
-  
+        id: "3ca09142-593e-4ea7-b0ec-9b840e3fbb35",
+        video: '/videos/code.mp4',
+      }
+    ],
+  },
+  {
+    username: "instagram",
+    stories: [
+      {
+        id: "c9464dc4-1643-44ef-aa9a-978b3071c992",
+        video: '/videos/cute.mp4',
+      },
+    ],
+  },
+  {
+    username: "icc",
+    stories: [
+      {
+        id: "c7bd8411-d6b8-4e7d-a620-74117409e3e2",
+        video: '/videos/icc.mp4',
+      },
+    ],
+  },
+  {
+    username: "pk",
+    stories: [
+      {
+        id: "5185e270-a319-4303-9624-c94f0fe5d2a6",
+        video: '/videos/frontend-course.mp4',
+      },
+    ],
+  },
+];
+
+export default cards;
