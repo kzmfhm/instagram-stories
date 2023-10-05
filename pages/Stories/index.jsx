f30fbc30f1c07fb295ababca1fdd9b76bc0d8178
kzmfhm <khuzaimanazik4@gmail.com>
2023-10-05 11:05:44 +0500

    Create responsiveness for StoryCards

--- pages/Stories/index.jsx
+++ pages/Stories/index.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import Appbar from '../../components/Layout/Appbar'
 import StoryContainer from '../../components/Story/StoryContainer'
-
 import './style.css'
 
 const Stories = () => {
   return (
     <div>
         <Appbar/>
-     
         <StoryContainer/>
-       
-      
-     
     </div>
   )
 }
